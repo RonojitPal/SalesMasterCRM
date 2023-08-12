92e44cf8331a5d9253119edbee05f6aa0a219808
Ronojit Pal <ronojit990@gmail.com>
2023-08-12 06:41:32 +0530

    register

--- management/urls.py
+++ management/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from management import views
 
 urlpatterns = [
+    path('',views.home,name='home'),
+    #path('login_user', views.login_user, name='loginuser'),
+    path('logout_user',views.logout_user,name='logoutuser'),
+    path('register_user',views.register_user,name='registeruser')
+
+
+
     
 ]
--- management/views.py
+++ management/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
+def home(request):
 
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=authenticate(request, username= username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "Error Logging In, Please Try Again!")
+            return redirect('home')
+    else:
+        return render(request, 'homepage.html', {})
+
+#def login_user(request):
+    #return render(request,)
+
+
+def logout_user(request):
+
+    logout(request)
+    messages.success(request, "You have been Logged Out!")
+    return redirect('home')
+
+
+def register_user(request):
+
+    return render(request, 'register.html', {})
+
+
+
+    
